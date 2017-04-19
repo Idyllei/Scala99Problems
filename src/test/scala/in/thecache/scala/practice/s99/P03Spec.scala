2e9f030c046d3a43b5677be5f1cb205a32725a08
Spencer Tupis <Idyllei@users.noreply.github.com>
2017-04-19 11:17:22 -0400

    Refactored.
    Made it follow DRY mantra. Made `lstInt` a fixture instead of repeatedly instantiating it for each test.
    
    Signed-off-by: Spencer Tupis <Idyllei@users.noreply.github.com>

--- src/test/scala/in/thecache/scala/practice/s99/P03Spec.scala
+++ src/test/scala/in/thecache/scala/practice/s99/P03Spec.scala
@@ -8,24 +8,49 @@ import org.scalatest._
   * Created by Spencer on 18-Apr-17.
   */
 class P03Spec extends FlatSpec {
+
+  private def lstInt = List[Int](0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
   behavior of "P03.nth"
 
   it should "get the nth element of a list" in {
-    val lstInt = List[Int](0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    assert(P03.nth(lstInt, 4) === 4)
+    val lst = lstInt
+    assert(P03.nth(lst, 4) === 4)
   }
 
-  it should "thrown a NoSuchElementException when given index > length of list" in {
-    val lstInt = List[Int](1, 2, 3, 4, 5)
+  it should "throw a NoSuchElementException when given index > length of list" in {
+    val lst = lstInt
     assertThrows[NoSuchElementException] {
-      P03.nth(lstInt, 5)
+      P03.nth(lst, 11)
     }
   }
 
   it should "throw a NoSuchElementException when given a negative index" in {
-    val lstInt = List[Int](1, 2, 3, 4, 5)
+    val lst = lstInt
     assertThrows[NoSuchElementException] {
-      P03.nth(lstInt, -1)
+      P03.nth(lst, -1)
     }
   }
+
+  behavior of "P03.nthBetter"
+
+  it should "get the nth element of a list" in {
+    val lst = lstInt
+    assert(P03.nthBetter(lst, 4) === 4)
+  }
+
+  it should "throw a NoSuchElementException when given index > length of list" in {
+    val lst = lstInt
+    assertThrows[NoSuchElementException] {
+      P03.nthBetter(lst, 11)
+    }
+  }
+
+  it should "throw a NoSuchElementException when given index < 0" in {
+    assertThrows[NoSuchElementException] {
+      val lst = lstInt
+      P03.nthBetter(lst, -1)
+    }
+  }
+
 }
