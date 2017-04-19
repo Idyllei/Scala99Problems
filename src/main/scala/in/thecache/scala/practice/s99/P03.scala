b546f17eb40697e310b96de0f99282ba58f4a3ab
Spencer Tupis <Idyllei@users.noreply.github.com>
2017-04-19 13:08:31 -0400

    Refactored test and included the given answer to the problem.
    Signed-off-by: Spencer Tupis <Idyllei@users.noreply.github.com>

--- src/main/scala/in/thecache/scala/practice/s99/P03.scala
+++ src/main/scala/in/thecache/scala/practice/s99/P03.scala
@@ -13,4 +13,29 @@ object P03 {
     if (n < 0) throw new NoSuchElementException
     nthWithCount(lst, n, 0)
   }
+
+  def nthBetter[A](lst: List[A], n: Int): A = {
+    /**
+      *
+      * @param n the index of the desired element
+      * @param lst the list holding the element to be fetched
+      * @return the `n`th element (of type `A`) in `lst`
+      */
+    def nthRecursive(n: Int, lst: List[A]): A = (n, lst) match {
+      case (0, x :: _ ) =>
+        // We are at the n-th element.
+        x
+      case (c, _ :: xs) =>
+        // we are 1 element closer to the n-th element; repeat on the remaining
+        // list.
+        nthRecursive(c - 1, xs)
+      case (_, Nil) => // End of list.
+        // End of list reached.
+        // This replaces the `case _ => ...` of `nthWithCount` and
+        // the `(n < 0)` check of `nth`.
+        throw new NoSuchElementException
+    }
+
+    nthRecursive(n, lst)
+  }
 }
--- src/test/scala/in/thecache/scala/practice/s99/P03Spec.scala
+++ src/test/scala/in/thecache/scala/practice/s99/P03Spec.scala
@@ -7,50 +7,88 @@ import org.scalatest._
 /**
   * Created by Spencer on 18-Apr-17.
   */
-class P03Spec extends FlatSpec {
+class P03Spec extends FlatSpec with Matchers with GivenWhenThen with AppendedClues {
 
   private def lstInt = List[Int](0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
   behavior of "P03.nth"
 
-  it should "get the nth element of a list" in {
+  it must "return the nth element of a list" in {
+    Given("a list whose fifth element (index = 4) is 4")
     val lst = lstInt
+
+    When("nth is called with an index of 4 (meaning the fifth element)")
+
+    Then("the result is 4")
     assert(P03.nth(lst, 4) === 4)
   }
 
-  it should "throw a NoSuchElementException when given index > length of list" in {
+  it must "throw an exception when the index is larger than the largest valid index of the list" in {
+    Given("a list and an index")
     val lst = lstInt
-    assertThrows[NoSuchElementException] {
-      P03.nth(lst, 11)
-    }
+    val invalidIndex = lst.last + 1 // lst.length
+
+    When("the index is greater than the largest valid index")
+
+    Then("an exception is thrown")
+    a [NoSuchElementException] should be thrownBy {
+      P03.nth(lst, invalidIndex)
+    } withClue (s"; P03.nth was given a list with length ${lst.length}"
+        + s" (this means it had a last valid index of ${lst.last},"
+        + s" and an index of $invalidIndex")
   }
 
-  it should "throw a NoSuchElementException when given a negative index" in {
+  it must "throw an exception when the index is negative" in {
+    Given("a list and a index")
     val lst = lstInt
-    assertThrows[NoSuchElementException] {
-      P03.nth(lst, -1)
-    }
+    val invalidIndex = -1
+
+    When("the index is negative")
+
+    Then("an exception is thrown")
+    a [NoSuchElementException] should be thrownBy {
+      P03.nth(lst, invalidIndex)
+    } withClue (s"; P03.nth was given a list of length ${lst.length}"
+        + s" and an index of $invalidIndex")
   }
 
   behavior of "P03.nthBetter"
 
-  it should "get the nth element of a list" in {
+  it must "return the nth element of a list" in {
+    Given("a list with a fifth element (index = 4) of 4")
     val lst = lstInt
-    assert(P03.nthBetter(lst, 4) === 4)
+
+    When("P0.nthBetter is called with an index of 4")
+    val index = 4
+
+    Then("the result is 4")
+    assert(P03.nthBetter(lst, index) === 4)
   }
 
-  it should "throw a NoSuchElementException when given index > length of list" in {
+  it must "throw an exception when the index is greater than the largest valid index of the list" in {
+    Given("a list of length n and and index")
     val lst = lstInt
-    assertThrows[NoSuchElementException] {
-      P03.nthBetter(lst, 11)
-    }
+
+    When("the index is greater than the last valid index (e.g., idx > n - 1)")
+    val invalidIndex = lst.last + 1
+
+    Then("an exception is thrown")
+    a [NoSuchElementException] should be thrownBy {
+      P03.nthBetter(lst, invalidIndex)
+    } withClue (s"; the last valid index of the list was ${lst.last}"
+      + s" but the requested index was $invalidIndex.")
   }
 
-  it should "throw a NoSuchElementException when given index < 0" in {
-    assertThrows[NoSuchElementException] {
-      val lst = lstInt
-      P03.nthBetter(lst, -1)
-    }
-  }
+  it must "throw an exception when the index is negative" in {
+    Given("a list and an index")
+    val lst = lstInt
 
+    When("the index is negative")
+    val invalidIndex = -1
+
+    Then("an exception is thrown")
+    a [NoSuchElementException] should be thrownBy {
+      P03.nthBetter(lst, invalidIndex)
+    } withClue (s"; the requested index was $invalidIndex")
+  }
 }
